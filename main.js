45158aa14c3a5ea8fd98f6b18c308f053b22f77a
jamie <skiletro@users.noreply.github.com>
2022-12-19 05:46:31 +0000

    initial html design

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 146 * 4,
+        height: 180 * 4,
         transparent: true,
         frame: false
     })
